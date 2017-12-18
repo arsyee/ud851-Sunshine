40617a69e86434e3962909682b2f8436de5d37d3
arsyee <2476948+arsyee@users.noreply.github.com>
2017-12-18 01:38:35 +0100

    S06.01 - settings was added to the menu in both activities

---
+++ S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.sunshine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        // Do step 2 in SettingsActivity
+        // COMPLETED (2) Set setDisplayHomeAsUpEnabled to true on the support ActionBar
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
