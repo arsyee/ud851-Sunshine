048e3ab49fb3d74b9e2ef2b137e06a149ce736ba
arsyee <2476948+arsyee@users.noreply.github.com>
2017-12-18 01:50:14 +0100

    S06.01 - fixed SettingsActivity's up navigation

--- S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/SettingsActivity.java
+++ S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/SettingsActivity.java
@@ -2,6 +2,7 @@ package com.example.android.sunshine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -12,5 +13,13 @@ public class SettingsActivity extends AppCompatActivity {
         // Do step 2 in SettingsActivity
         // COMPLETED (2) Set setDisplayHomeAsUpEnabled to true on the support ActionBar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
