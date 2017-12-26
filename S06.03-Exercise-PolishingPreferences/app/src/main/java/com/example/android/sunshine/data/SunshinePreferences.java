ca4f2e1199955db43af42b60fc930c4cd6ed995a
arsyee <2476948+arsyee@users.noreply.github.com>
2017-12-26 19:24:20 +0100

    S06.03 - my solutions

--- S06.03-Exercise-PolishingPreferences/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
+++ S06.03-Exercise-PolishingPreferences/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
@@ -16,9 +16,16 @@
 package com.example.android.sunshine.data;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.example.android.sunshine.R;
+import com.example.android.sunshine.utilities.SunshineWeatherUtils;
 
 public class SunshinePreferences {
 
+    private static final String TAG = SunshinePreferences.class.getSimpleName();
     /*
      * Human readable location string, provided by the API.  Because for styling,
      * "Mountain View" is more recognizable than 94043.
@@ -87,9 +94,11 @@ public class SunshinePreferences {
      * "94043,USA" if SharedPreferences have not been implemented yet.
      */
     public static String getPreferredWeatherLocation(Context context) {
-        // TODO (1) Return the user's preferred location
-        /** This will be implemented in a future lesson **/
-        return getDefaultWeatherLocation();
+        // COMPLETED (1) Return the user's preferred location
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        String preferredLocation = sharedPreferences.getString(context.getString(R.string.pref_location_key), DEFAULT_WEATHER_LOCATION);
+        Log.d(TAG, "getPreferredWeatherLocation returns: " + preferredLocation);
+        return preferredLocation;
     }
 
     /**
@@ -100,9 +109,13 @@ public class SunshinePreferences {
      * @return true If metric display should be used
      */
     public static boolean isMetric(Context context) {
-        // TODO (2) Return true if the user's preference for units is metric, false otherwise
-        /** This will be implemented in a future lesson **/
-        return true;
+        // COMPLETED (2) Return true if the user's preference for units is metric, false otherwise
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        boolean isMetric = sharedPreferences.getString(context.getString(R.string.pref_units_key),
+                                                       context.getString(R.string.pref_units_metric)).
+                                             equals(context.getString(R.string.pref_units_metric));
+        Log.d(TAG, "isMetric returns " + isMetric);
+        return isMetric;
     }
 
     /**
